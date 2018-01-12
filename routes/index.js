2871477949e7bac35dfae950242af43a4aa681f3
Mateus Mourao <mmdfmateus@gmail.com>
2018-01-12 13:49:27 -0200

    excel

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var firebase = require("firebase");
+var xl = require('excel4node');
 
 var router = express.Router();
 
@@ -44,6 +45,33 @@ router.post("/items", function(req, res, next) {
     .push(req.body);
 
   res.json(req.body);
+});
+
+router.post("/close", function(req, res, next) {
+  var identity = parseIdentity(req.body.identity);
+
+  var viagem = firebase
+                .database()
+                .ref("travel/" + identity)
+                .toJSON();
+  
+  var wb = new xl.Workbook();
+
+  var style = wb.createStyle({
+    font: {
+        color: '#FF0800',
+        size: 12
+    },
+    numberFormat: '$#,##0.00; ($#,##0.00); -'
+  });
+
+  var ws = wb.addWorksheet('Sheet 1');
+
+  ws.cell(1,1).string('teste1').style(style);
+  ws.cell(1,2).string('teste2').style(style);
+  wb.write('Excel.xlsx');
+
+  res.json('req.body');
 });
 
 module.exports = router;
