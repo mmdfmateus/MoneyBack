248551cba6db8101ffc5cfd7975bb37dccf5bc2d
Mateus Mourao <mmdfmateus@gmail.com>
2018-01-12 10:18:56 -0200

    init

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var firebase = require("firebase");
+
+var router = express.Router();
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCCitS6JiGyv26-eB7VuuQisyrr8LTRkc0",
+  authDomain: "moneyback-60e24.firebaseapp.com",
+  databaseURL: "https://moneyback-60e24.firebaseio.com",
+  projectId: "moneyback-60e24",
+  storageBucket: "moneyback-60e24.appspot.com",
+  messagingSenderId: "677380462491"
+};
+
+firebase.initializeApp(config);
+
+/* GET home page. */
+router.post('/:user', function(req, res, next) {
+  firebase
+    .database()
+    .ref("travel/" + req.params.user)
+    .set({
+    });
+
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
