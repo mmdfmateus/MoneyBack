d806c48e2be328c7b840bea9869e940ff70a2e09
Breno Queiroz <brenoqueiroz@gmail.com>
2018-01-12 14:53:40 -0200

    email'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var firebase = require("firebase");
-var xl = require('excel4node');
+var xl = require("excel4node");
+var nodemailer = require("nodemailer");
 
 var router = express.Router();
 
@@ -61,28 +62,156 @@ router.post("/items", function(req, res, next) {
 router.post("/close", function(req, res, next) {
   var identity = parseIdentity(req.body.identity);
 
-  var viagem = firebase
-                .database()
-                .ref("travel/" + identity)
-                .toJSON();
-  
-  var wb = new xl.Workbook();
+  firebase
+    .database()
+    .ref("travel/" + identity)
+    .once("value")
+    .then(function(snapshot) {
+      var viagem = snapshot.val();
+      var wb = new xl.Workbook();
 
-  var style = wb.createStyle({
-    font: {
-        color: '#FF0800',
-        size: 12
-    },
-    numberFormat: '$#,##0.00; ($#,##0.00); -'
-  });
+      var headerStyle = wb.createStyle({
+        font: {
+          color: "#212121",
+          bold: true,
+          size: 14
+        }
+      });
 
-  var ws = wb.addWorksheet('Sheet 1');
+      var style = wb.createStyle({
+        font: {
+          color: "#212121",
+          size: 12
+        }
+      });
 
-  ws.cell(1,1).string('teste1').style(style);
-  ws.cell(1,2).string('teste2').style(style);
-  wb.write('Excel.xlsx');
+      var ws = wb.addWorksheet("Reembolso");
 
-  res.json('req.body');
+      ws
+        .cell(1, 1)
+        .string("Data inicio")
+        .style(headerStyle);
+
+      ws
+        .cell(2, 1)
+        .string(viagem.dataInicio)
+        .style(style);
+
+      ws
+        .cell(1, 2)
+        .string("Data fim")
+        .style(headerStyle);
+
+      ws
+        .cell(2, 2)
+        .string(viagem.dataFim)
+        .style(style);
+
+      ws
+        .cell(1, 3)
+        .string("Destino")
+        .style(headerStyle);
+
+      ws
+        .cell(2, 3)
+        .string(viagem.destino)
+        .style(style);
+
+      ws
+        .cell(1, 4)
+        .string("Centro de custo")
+        .style(headerStyle);
+
+      ws
+        .cell(2, 4)
+        .string(viagem.centroDeCusto)
+        .style(style);
+
+      ws
+        .cell(1, 5)
+        .string("Solicitante")
+        .style(headerStyle);
+
+      ws
+        .cell(2, 5)
+        .string(viagem.nome)
+        .style(style);
+      ws
+        .cell(4, 1)
+        .string("Natureza")
+        .style(headerStyle);
+
+      ws
+        .cell(4, 2)
+        .string("Valor")
+        .style(headerStyle);
+
+      ws
+        .cell(4, 3)
+        .string("Nota fiscal")
+        .style(headerStyle);
+
+      var keys = Object.keys(viagem.items);
+      for (var i = 0; i < keys.length; i++) {
+        var item = viagem.items[keys[i]];
+
+        ws
+          .cell(5 + i, 1)
+          .string(item.natureza)
+          .style(style);
+
+        ws
+          .cell(5 + i, 2)
+          .number(parseFloat(item.valorGasto))
+          .style(
+            wb.createStyle({
+              font: {
+                color: "#212121",
+                size: 12
+              },
+              numberFormat: "R$#,##0.00; (R$#,##0.00); -"
+            })
+          );
+
+        ws
+          .cell(5 + i, 3)
+          .string(item.notaFiscal)
+          .style(style);
+      }
+
+      wb.writeToBuffer().then(function(buffer) {
+        var transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "moneybackbot@gmail.com",
+            pass: "moneybackpass"
+          }
+        });
+
+        var mailOptions = {
+          from: "moneybackbot@gmail.com",
+          to: "brenof@take.net",
+          subject: "Solicitação de reembolso - " + viagem.nome,
+          text: "Segue despesas em anexo",
+          attachments: [
+            {
+              filename: "solicitacao-reembolso.xlsx",
+              content: buffer
+            }
+          ]
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+      });
+
+      res.json("done");
+    });
 });
 
 module.exports = router;
